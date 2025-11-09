7defaa8631358a6ca341c12e178ee2c941c21cd0
GenerallyBlinky <59066255+generallyblinkydev@users.noreply.github.com>
2025-11-09 16:54:22 +0000

    Update wiki-script.js

--- wiki-script.js
+++ wiki-script.js
@@ -2,4 +2,4 @@ window.addEventListener('load', function () {
   document.getElementById('wiki-search').oninput = function(){
     document.getElementById('test').textcontent = document.getElementById('wiki-search').value
   };
-};
+});
