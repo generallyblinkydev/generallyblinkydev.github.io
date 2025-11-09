f7149bab1dc85d4d063473eb6d1538cae24b6cfd
GenerallyBlinky <59066255+generallyblinkydev@users.noreply.github.com>
2025-11-09 17:32:30 +0000

    Update wiki-script.js

--- wiki-script.js
+++ wiki-script.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', function () {
   document.getElementById('wiki-search').oninput = function(){
-    document.getElementById('test').innerHTML = document.getElementById('wiki-search').value
     fetch('/wikis/bloodloss/test-page.txt')
       .then(response => response.text())
       .then(data => document.getElementById('page').innerHTML = data);
