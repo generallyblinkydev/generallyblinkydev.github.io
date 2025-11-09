90e4d63676655741a5de460d67d8d91303401df4
GenerallyBlinky <59066255+generallyblinkydev@users.noreply.github.com>
2025-11-09 17:13:34 +0000

    Update wiki-script.js

--- wiki-script.js
+++ wiki-script.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', function () {
   document.getElementById('wiki-search').oninput = function(){
     document.getElementById('test').innerHTML = document.getElementById('wiki-search').value
+    fetch('/wikis/bloodloss/test-page.txt')
+      .then(response => response.text())
+      .then(data => console.log(data));
   };
 });
